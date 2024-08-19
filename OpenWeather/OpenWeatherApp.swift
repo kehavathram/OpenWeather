bb4bcf41185f3b56d9f7b082430e28c0e220b084
Thukaram Kethavath <thukaram.kethavath@stryker.com>
2024-08-19 09:57:22 -0500

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Thukaram Kethavath on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
